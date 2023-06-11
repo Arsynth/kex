201b410f4e72145f2009e99640a58432168e50fa
Arsynth <arsynthdev@gmail.com>
2023-06-12 04:59:12 +0700

    New test cases

--- src/format/byte.rs
+++ src/format/byte.rs
@@ -57,8 +57,14 @@ impl ByteFormatting for ByteFormatter {
             byte_number += to_format;
 
             if to_format != 0 {
-                for byte in &tmp[..to_format] {
-                    result += &format!("{:02x}", byte);
+                if self.is_little_endian {
+                    for byte in tmp[..to_format].iter().rev() {
+                        result += &format!("{:02x}", byte);
+                    }
+                } else {
+                    for byte in &tmp[..to_format] {
+                        result += &format!("{:02x}", byte);
+                    }
                 }
             }
 
@@ -80,12 +86,11 @@ impl ByteFormatting for ByteFormatter {
         while tmp != 0 {
             let to_format = min(tmp, gr.bytes_left_in_group_after(byte_number));
 
-            
             let needs_separator = byte_number != 0 && gr.is_aligned_at(byte_number);
             if needs_separator {
                 result += &sep;
             }
-            
+
             byte_number += to_format;
 
             if to_format != 0 {
@@ -116,7 +121,7 @@ mod tests {
             }
         }
 
-        fn run(&self, fmt: &mut ByteFormatter) {
+        fn run(&self, fmt: &ByteFormatter) {
             let mut out = String::new();
             let mut num = 0usize;
             for part in self.parts.iter() {
@@ -134,25 +139,67 @@ mod tests {
 
     #[test]
     fn test_unordered() {
-        let mut fmt = ByteFormatter::new(Groupping::BytesPerRow(4, Group::new(4, "")), false);
+        let fmt = ByteFormatter::new(Groupping::BytesPerRow(4, Group::new(4, "")), false);
+        let cases = vec![Case::new(
+            vec![vec![0xfeu8, 0xed, 0xfa], vec![0xce]],
+            "feedface",
+        )];
+        for case in cases {
+            case.run(&fmt);
+        }
+
+        let fmt = ByteFormatter::new(Groupping::BytesPerRow(8, Group::new(4, " ")), false);
         let cases = vec![
-            Case::new(vec![vec![0xfeu8, 0xed, 0xfa], vec![0xce]], "feedface")
+            Case::new(
+                vec![vec![0xfeu8, 0xed, 0xfa], vec![0xce]],
+                "feedface ........",
+            ),
+            Case::new(
+                vec![vec![0xfeu8], vec![0xed, 0xfa, 0xce]],
+                "feedface ........",
+            ),
+            Case::new(
+                vec![vec![0xfeu8], vec![0xed], vec![0xfa, 0xce]],
+                "feedface ........",
+            ),
+            Case::new(
+                vec![vec![0xfeu8, 0xed, 0xfa], vec![0xce, 0xca]],
+                "feedface ca......",
+            ),
+            Case::new(
+                vec![vec![0xfeu8], vec![0xed, 0xfa, 0xce, 0xca]],
+                "feedface ca......",
+            ),
+        ];
+
+        for case in cases {
+            case.run(&fmt);
+        }
+
+        let fmt = ByteFormatter::new(Groupping::RepeatingGroup(Group::new(4, "-"), 4), true);
+        let cases = vec![
+            Case::new(
+                vec![vec![
+                    0xfeu8, 0xed, 0xfa, 0xce, 0xfe, 0xed, 0xfa, 0xce, 0xfe, 0xed, 0xfa, 0xce, 0xfe,
+                    0xed, 0xfa, 0xce,
+                ]],
+                "cefaedfe-cefaedfe-cefaedfe-cefaedfe",
+            ),
+            Case::new(
+                vec![vec![
+                    0xfeu8, 0xed, 0xfa, 0xce, 0xfe, 0xed, 0xfa, 0xce
+                ]],
+                "cefaedfe-cefaedfe-........-........",
+            ),
+            Case::new(
+                vec![vec![
+                    0xfeu8, 0xed, 0xfa, 0xce, 0xfe, 0xed, 0xfa, 0xce, 0xfe, 0xed
+                ]],
+                "cefaedfe-cefaedfe-edfe....-........",
+            ),
         ];
         for case in cases {
-            case.run(&mut fmt);
-        }
-        
-        let mut fmt = ByteFormatter::new(Groupping::BytesPerRow(8, Group::new(4, " ")), false);
-        let cases = vec![
-            Case::new(vec![vec![0xfeu8, 0xed, 0xfa], vec![0xce]], "feedface ........"),
-            Case::new(vec![vec![0xfeu8], vec![0xed, 0xfa, 0xce]], "feedface ........"),
-            Case::new(vec![vec![0xfeu8], vec![0xed], vec![0xfa, 0xce]], "feedface ........"),
-            Case::new(vec![vec![0xfeu8, 0xed, 0xfa], vec![0xce, 0xca]], "feedface ca......"),
-            Case::new(vec![vec![0xfeu8], vec![0xed, 0xfa, 0xce, 0xca]], "feedface ca......"),
-            ];
-        
-        for case in cases {
-            case.run(&mut fmt);
+            case.run(&fmt);
         }
     }
 
